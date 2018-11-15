34716614dee0f07deeb21195c4477781a3708c79
up209d <up209d@gmail.com>
2018-11-15 15:36:48 +1100

    Migrates isBreakPointUp/Down from Redux action to Material UI Theme !

--- src/js/reducers/uiReducer.js
+++ src/js/reducers/uiReducer.js
@@ -1,11 +1,18 @@
 import actionTypes from 'js/actionTypes';
 import { createMuiTheme } from '@material-ui/core';
 import themes from 'js/themes';
+import utils from 'js/utils';
 
 export const uiInitialState = {
   theme: {
     ...createMuiTheme(themes.default),
-    currentBreakpoint: 'md'
+    currentBreakpoint: 'md',
+    isBreakpointUp: function(breakpoint) {
+      return utils.isBreakpointUp(breakpoint,this.currentBreakpoint)
+    },
+    isBreakpointDown: function(breakpoint) {
+      return utils.isBreakpointDown(breakpoint,this.currentBreakpoint)
+    }
   },
   alert: {
     type: null,
